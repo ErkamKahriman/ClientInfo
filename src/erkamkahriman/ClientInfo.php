1e63ea780faccd4fdc9fadb30a5e1730424dff4a
Erkam Kahriman <erkamkahriman@gmail.com>
2018-01-30 18:00:31 +0100

    Added More Info and more details

--- src/erkamkahriman/ClientInfo.php
+++ src/erkamkahriman/ClientInfo.php
@@ -11,7 +11,7 @@ class ClientInfo extends PluginBase {
     public function onEnable(){
         $this->getServer()->getCommandMap()->register("cinfo", new ClientInfoCommand("cinfo", $this));
         $this->getServer()->getCommandMap()->getCommand("cinfo")->getOverload("default")->setParameter(0, new CommandParameter("spieler", CommandParameter::TYPE_TARGET, false));
-        $this->getServer()->getCommandMap()->getCommand("cinfo")->setDescription("Get Informations about a Client");
+        $this->getServer()->getCommandMap()->getCommand("cinfo")->setDescription("Get Informations about a Client.");
         $this->getServer()->getCommandMap()->getCommand("cinfo")->setAliases(array("clientinfo"));
         $this->getLogger()->info(C::GREEN . "Enabled.");
     }
--- src/erkamkahriman/ClientInfoCommand.php
+++ src/erkamkahriman/ClientInfoCommand.php
@@ -30,9 +30,11 @@ class ClientInfoCommand extends PluginCommand {
                     $spieler = $this->plugin->getServer()->getPlayer(strtolower($args[0]));
                     if ($spieler->isOnline()) {
                         $sender->sendMessage(self::CINFO);
+                        $sender->sendMessage(C::YELLOW . "Username: " . C::WHITE . $spieler->getName());
+                        $sender->sendMessage(C::YELLOW . "NameTag: " . C::RESET . $spieler->getNameTag());
                         $sender->sendMessage(C::YELLOW . "SystemOS: " . C::WHITE . $this->getOS($spieler));
                         $sender->sendMessage(C::YELLOW . "Device: " . C::WHITE . $spieler->getDeviceModel());
-                        $sender->sendMessage(C::YELLOW . "Language: " . C::WHITE . $spieler->getLocale());
+                        $sender->sendMessage(C::YELLOW . "Language: " . C::WHITE . $this->getLanguage($spieler));
                         $sender->sendMessage(C::YELLOW . "IP: " . C::WHITE . $spieler->getAddress());
                     } else {
                         $sender->sendMessage(self::PLAYERNOTON);
@@ -60,6 +62,84 @@ class ClientInfoCommand extends PluginCommand {
                 return "FireOS";
             case 7:
                 return "Windows";
+            default:
+                return "Unknown";
+        }
+    }
+
+    public function getLanguage(Player $player){
+        switch ($player->getLocale()){
+            case "af_ZA":
+                return "Afrikaans";
+            case "ar_SA":
+                return "Arabic";
+            case "ast_ES":
+                return "Asturian";
+            case "az_AZ":
+                return "Azerbaijani";
+            case "be_BY":
+                return "Belarusian";
+            case "bg_BG":
+                return "Bulgarian";
+            case "br_FR":
+                return "Breton";
+            case "ca_ES":
+                return "Catalan";
+            case "cs_CZ":
+                return "Czech";
+            case "cy_GB":
+                return "Welsh";
+            case "da_DK":
+                return "Danish";
+            case "de_AT":
+                return "Austrian German";
+            case "de_DE":
+                return "German";
+            case "el_GR":
+                return "Greek";
+            case "en_AU":
+                return "Australian English";
+            case "en_CA":
+                return "Canadian English";
+            case "en_GB":
+                return "British English";
+            case "en_NZ":
+                return "New Zealand English";
+            case "en_UD":
+                return "British English (upside down)";
+            case "en_7S":
+                return "Pirate English";
+            case "en_US":
+                return "American English";
+            case "eo_UY":
+                return "Esperanto";
+            case "es_AR":
+                return "Argentinian Spanish";
+            case "es_ES":
+                return "Spanish";
+            case "es_MX":
+                return "Mexican Spanish";
+            case "es_UY":
+                return "Uruguayan Spanish";
+            case "es_VE":
+                return "Venezuelan Spanish";
+            case "et_EE":
+                return "Estonian";
+            case "eu_ES":
+                return "Basque";
+            case "fa_IR":
+                return "Persian";
+            case "fi_FI":
+                return "Finnish";
+            case "fil_PH":
+                return "Filipino";
+            case "fo_FO":
+                return "Faroese";
+            case "fr_FR":
+                return "French";
+            case "fr_CA":
+                return "Canadian French";
+                //TODO More Languages
             default:
                 return "Unknown";
         }
